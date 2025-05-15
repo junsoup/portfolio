072ab00cb6c00771c3c1e1df1cc9120dacd5c44c
Junsu Lee <61336401+junsoup@users.noreply.github.com>
2025-05-14 19:37:09 -0700

    Blog post on league AI project.

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ function toggleMenu() {
         x.style.maxHeight = num_links * navHeight + "px";
     } else {
         x.style.maxHeight = navHeight + "px";
-        setTimeout(function(){
+        setTimeout(function () {
             x.className = "";
         }, 300);
     }
 }
-
-document.getElementById("toggleButton").addEventListener("click", function (event) {
-    event.stopPropagation();
-    const banner = document.getElementById("banner");
-    // banner.classList.toggle("expanded");
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-        return;
-      }
-      // Make the .element div fullscreen.
-      banner.requestFullscreen();
-});
+if (document.getElementById("toggleButton") !== null) {
+    document.getElementById("toggleButton").addEventListener("click", function (event) {
+        event.stopPropagation();
+        const banner = document.getElementById("banner");
+        // banner.classList.toggle("expanded");
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+            return;
+        }
+        // Make the .element div fullscreen.
+        banner.requestFullscreen();
+    });
+}
